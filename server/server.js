63b532caf451b7fb5b02a409f5b2226e83a1b9bc
ZoranK <zorankarbev@gmail.com>
2022-09-07 17:05:29 +0200

    added mongoDB connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose")
 const globalRouter = require("./const/router")
-const app = express();
 const cors = require("cors");
 
+const MONGO_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.716de.mongodb.net/?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST | "0.0.0.0"
+
+const app = express();
 
 app.get('/', function (req, res) {
   res.send('TEST1')
@@ -17,6 +20,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use("/api", globalRouter);
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server is up and running on PORT: ${PORT}`)
+mongoose.connect(MONGO_URI, err => {
+  if (err) return console.log(err);
+
+  console.log("Connected to MongoDB");
+
+  app.listen(PORT, HOST, () => {
+    console.log(`Server is up and running on PORT: ${PORT}`)
+  });
 });
